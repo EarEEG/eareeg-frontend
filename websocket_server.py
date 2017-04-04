fcd90553255edb147608e192fa1de1f3a565942e
Tyler Stuessi <tyler.stuessi@gmail.com>
2017-04-04 13:18:33 -0400

    Add basic socketIO server

--- websocket_server.py
+++ websocket_server.py
@@ -3,11 +3,28 @@ Basic Flask server
 '''
 
 from flask import Flask
+from flask_socketio import SocketIO, send, emit
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 @app.route("/")
 def index():
     return "Hello World!"
 
+@socketio.on("data")
+def handle_data(json):
+    print("Recieved JSON:\n", json)
+    emit("data", json, broadcast=True)
+
+@socketio.on('connect')
+def test_connect():
+    print("Connected to host")
+    emit('connected', {'data': 'Connected'})
+
+@socketio.on('disconnect')
+def test_disconnect():
+    print('Client disconnected')
+
+
 if __name__ == "__main__":
-    app.run()
+    socketio.run(app)
