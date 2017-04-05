40daae6763858c652fa9a773acfef8d78bd3f9ff
Jeremy Herwig <jherwig1@vols.utk.edu>
2017-04-05 13:34:11 -0400

    Adds websocket server

---
+++ BottleServer.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+
+from bottle import Bottle
+from bottle.ext.websocket import websocket, GeventWebSocketServer
+
+from utils import get_data_path
+
+import json
+import os
+
+
+app = Bottle()
+
+
+@app.get('/ws/fetch', apply=[websocket])
+def fetch(ws):
+	"""Receives request for either replayed or simulation data."""
+	connection = True
+
+	while connection:
+		message = ws.receive()
+		if message is not None:
+			ws.send("{}")
+
+
+@app.get('/ws/load', apply=[websocket])
+def ws_init(ws):
+	"""Find requested simulation data and return Sysparam and Sysname, else return error."""
+	connection = True
+
+	print "ws_init"
+	while connection:
+		message = ws.receive()
+		rv = {}
+		if message is not None:
+			query = json.loads(message)
+			run_id = str(query['id'])
+			path = data_dir
+			run_path = os.path.join(path, run_id)
+			sys_path = os.path.join(run_path, 'config.json')
+
+			# Find simulation and return Sysparam and Sysname
+			if os.path.exists(sys_path):
+				try:
+					with open(sys_path, 'r') as param_fp:
+						rv['config'] = json.loads(param_fp.read())
+				except:
+					rv['error'] = "No Config found"
+
+			# Send through websocket whether requested sim is running
+			# Find way to tell this
+			else:
+				if True:
+					rv['Status'] = "Simulation running"
+				else:
+					rv['error'] = "Simulation not running"
+			ws.send(json.dumps(rv))
+
+		else:  # message is None
+			connection = False
+
+if __name__ == "__main__":
+	data_dir = get_data_path()
+	app.run(host='cepsltb7.curent.utk.edu', port='9121', server=GeventWebSocketServer)
